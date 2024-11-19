1f8e8d273774815d07f7ceb2e994a0ccac7bc7e1
Sergio Alfonso <sergioluisalfonso@gmail.com>
2024-11-19 05:14:56 -0500

    feat: config envs

--- src/fetchers/orders.fetchers.ts
+++ src/fetchers/orders.fetchers.ts
@@ -1,6 +1,6 @@
+import { getServiceUrl } from "@/config/utils/env.utils";
 import { orderResponseToOrders } from "@/modules/orders/mappers/orders.mappers";
 import { Order } from "@/modules/orders/types/orders.types";
-import { getServiceUrl } from "@/utils/env.utils";
 
 export const fetchOrder = (order: number): Promise<Order> => {
   const URL_SERVICE = `orders`;
