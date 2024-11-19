2fff927e04bbdb414aed0932b746129d437b78bd
Sergio Alfonso <sergioluisalfonso@gmail.com>
2024-11-19 04:15:46 -0500

    feat: filter stores by is open

--- src/components/toggle/toggle.tsx
+++ src/components/toggle/toggle.tsx
@@ -1,15 +1,24 @@
-import { FC, ReactNode } from "react";
+import { ChangeEvent, FC, ReactNode } from "react";
 import styles from "./toggle.module.scss";
 
 type ToggleProps = {
   children: ReactNode;
   className?: string;
+  onChange?: (checked: boolean) => void;
 };
 
-const Toggle: FC<ToggleProps> = ({ children, className }) => {
+const Toggle: FC<ToggleProps> = ({ children, className, onChange }) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange && onChange(!!e.target.checked);
+  };
+
   return (
     <label className={`${styles.toggle} ${className || ""}`}>
-      <input type="checkbox" className={styles.checkbox} />
+      <input
+        type="checkbox"
+        className={styles.checkbox}
+        onChange={handleChange}
+      />
       {children}
     </label>
   );
