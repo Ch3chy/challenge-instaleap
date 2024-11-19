dd440ba0324d39f4fd616103418f318e30189596
Sergio Alfonso <sergioluisalfonso@gmail.com>
2024-11-19 04:08:47 -0500

    feat: show store in map when item is selected

--- src/modules/orders/components/store/store.tsx
+++ src/modules/orders/components/store/store.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { ChangeEvent, FC } from "react";
 import styles from "./store.module.scss";
 import Tag from "@/components/tag";
 import { Timer } from "@phosphor-icons/react/dist/ssr";
@@ -10,9 +10,10 @@ type StoreProps = {
   store: StoreType;
   name?: string;
   className?: string;
+  onChange?: (checked: boolean) => void;
 };
 
-const Store: FC<StoreProps> = ({ store, name, className }) => {
+const Store: FC<StoreProps> = ({ store, name, className, onChange }) => {
   const avatarText = getTextInitials(store.name);
 
   const times = {
@@ -20,11 +21,22 @@ const Store: FC<StoreProps> = ({ store, name, className }) => {
     to: dayjs(`1/1/1 ${store.nextDeliveryTime.to}`).format("hh:mm a"),
   };
 
+  const handleChangeStore = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange && onChange(!!e.target.checked);
+  };
+
   return (
     <article className={`${styles.store} ${className || ""}`}>
       {name && (
         <span className={styles.radio}>
-          <input name={name} type="radio" className={styles.radio} />
+          <input
+            name={name}
+            type="radio"
+            value={store.id}
+            className={styles.radio}
+            disabled={!store.isOpen}
+            onChange={handleChangeStore}
+          />
         </span>
       )}
       <div className={styles.avatar}>{avatarText}</div>
--- src/types/customers.types.ts
+++ src/types/customers.types.ts
@@ -1,4 +1,7 @@
+import { Coordinates } from "./map.types";
+
 export type Customer = {
   id: number;
   name: string;
+  coordinates: Coordinates;
 };
